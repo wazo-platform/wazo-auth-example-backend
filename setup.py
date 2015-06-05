34d221d2b9875362ce6431284fabbfd50de9d56f
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-06-05 10:18:24 -0400

    add a minimal setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='xivo_auth-example-backend',
+    version='0.0.1',
+
+    description='A simple example backend plugin for xivo-auth',
+
+    author='Avencall',
+    author_email='dev@avencall.com',
+
+    url='https://github.com/xivo-pbx/xivo-auth-example-backend',
+
+    packages=find_packages(),
+
+    entry_points={
+        'xivo_auth.backends': [
+            'example = example_backend.example:ExampleBackend',
+        ],
+    }
+)
